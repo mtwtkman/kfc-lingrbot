7a6e7cfd242853ea77592a0f5374b2964a234d0e
mtwtkman <punipuniomochi@gmail.com>
2014-12-16 23:19:15 +0900

    yeah

--- app.py
+++ app.py
@@ -1,14 +1,19 @@
 from flask import Flask, request
 import json
 
+from tori import tori
+
 app = Flask(__name__)
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
   if request.method == 'POST':
-    return 'receive'
+    if 'KFC' in request.json['message']['text']:
+      return 'torinohi'
+    else:
+      pass
   elif request.method == 'GET':
-    return 'hoge'
+    return 'toriniku'
 
 if __name__ == '__main__':
-  app.run()
+  app.run(debug=True)
---
+++ tori.py
@@ -0,0 +1,7 @@
+from bs4 import BeautifulSoup as bs
+from urllib.request import urlopen
+from datetime import datetime, date
+
+def tori():
+
+  pass
