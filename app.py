089762921f8718e8110dea725f26c0d0480ca868
mtwtkman <punipuniomochi@gmail.com>
2014-12-16 23:48:34 +0900

    good

--- app.py
+++ app.py
@@ -1,22 +1,22 @@
+# -*- coding: utf-8 -*-
+
 from flask import Flask, request
-#from bs4 import BeautifulSoup as bs
-#from urllib.request import urlopen
-#from datetime import datetime, date
-import json
+from datetime import datetime, date
 
 app = Flask(__name__)
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
   if request.method == 'POST':
-    msg = ''
     data = request.json
-    if data['status'] == 'ok' and data['events']:
-      msg = data['events'][0]['message']['text']
-    return msg
+    if data['status'] == 'ok' and 'KFC' in data['events'][0]['message']['text']:
+      return 'もうちょっと我慢しろ'
+    else:
+      pass
 
   elif request.method == 'GET':
     return 'toriniku'
+
 
 if __name__ == '__main__':
   app.run()
