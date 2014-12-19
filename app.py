5e3c4f874d193c75f907e8a815bebaf9cffde82d
mtwtkman <punipuniomochi@gmail.com>
2014-12-19 22:51:24 +0900

    amend msg file

--- app.py
+++ app.py
@@ -12,12 +12,14 @@ from kfc_msg import kfc_msg
 
 app = Flask(__name__)
 app.config['DEBUG'] = True
-app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('HEROKU_POSTGRESQL_VIOLET_URL') if os.getenv('IS_HEROKU') else 'postgresql://boku:@localhost/toridb'
+app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('HEROKU_POSTGRESQL_VIOLET_URL') if os.getenv('IS_HEROKU') else os.getenv('POSTGRES_TORI')
 
 db = SQLAlchemy(app)
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
+  signal()
+
   if request.method == 'POST':
     data = request.json
 
@@ -31,6 +33,22 @@ def index():
   elif request.method == 'GET':
     return 'toriniku'
 
+def signal():
+  today = datetime.now(pytz.timezone('Asia/Tokyo'))
+
+  tori_day = 28
+  tori_day_signal = (0, 6, 12, 15, 18, 20)
+
+  before_day = 27
+  before_day_signal = (0, 6, 12, 15, 23)
+
+  if today.day is tori_day and today.minute is 0:
+    if today.hour in tori_day_signal:
+      return '今日は記念すべきとりの日です。満を持して行きましょう。'
+  elif today.day is before_day and today.minute is 0:
+    if today.hour in before_day_signal:
+      return '明日は待ちに待ったとりの日です。開店ダッシュしましょう。'
+
 def tori():
   today = datetime.now(pytz.timezone('Asia/Tokyo'))
   if today.day is 28:
@@ -43,13 +61,7 @@ def tori():
       last_day = monthrange(year, month)[1]
       left_days = timedelta(last_day - day + 28)
 
-    if left_days.days-1 > 20:
-      degree = 'far'
-    elif left_days.days-1 > 5:
-      degree = 'middle'
-    else:
-      degree = 'near'
-    return 'とりの日パックまであと{}日なんだけど{}'.format(left_days.days, random.choice(kfc_msg[degree]))
+    return 'とりの日パックまであと{}日なんだけど{}'.format(left_days.days, random.choice(kfc_msg))
 
 if __name__ == '__main__':
   app.run()
